4dc0984849a5823a45967a50b14813a797a35b27
Simphiwe <simphiwetebe@gmail.com>
2020-08-08 02:01:35 +0200

    Added Dynamic Product data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import data from './data'
 
 function App() {
 
@@ -28,54 +29,30 @@ function App() {
         <ul>
           <li><a href="index.html">Sneakers</a></li>
           <li><a href="index.html">Pants</a></li>
-
         </ul>
       </aside>
 
       <main className="main">
         <div className="content">
           <ul className="products">
-            <li>
-              <div className="product">
-                <img src="/images/bathu.webp" alt="" className="product__image" />
-                <a href="product.html" className="product__name">Journey Edition - Yellow</a>
-                <div className="product__brand">Bathu</div>
-                <div className="product__price">R1,700</div>
-                <div className="product__rating">4.5 stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img src="/images/bathu_green.webp" alt="" className="product__image" />
-                <a href="product.html" className="product__name">Journey Edition - Army Green</a>
-                <div className="product__brand">Bathu</div>
-                <div className="product__price">R1,700</div>
-                <div className="product__rating">4.5 stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img src="/images/bathu_red.webp" alt="" className="product__image" />
-                <a href="product.html" className="product__name">Journey Edition - Red</a>
-                <div className="product__brand">Bathu</div>
-                <div className="product__price">R1,700</div>
-                <div className="product__rating">4.5 stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img src="/images/bathu_red.webp" alt="" className="product__image" />
-                <a href="product.html" className="product__name">Journey Edition - Red</a>
-                <div className="product__brand">Bathu</div>
-                <div className="product__price">R1,700</div>
-                <div className="product__rating">4.5 stars (10 Reviews)</div>
-              </div>
-            </li>
+            {
+              data.products.map(product => (
+                <li key={product.id}>
+                  <div className="product">
+                    <img src={product.imageUrl} alt="" className="product__image" />
+                    <a href="product.html" className="product__name">{product.name}</a>
+                    <div className="product__brand">{product.brand}</div>
+                    <div className="product__price">{product.price}</div>
+                    <div className="product__rating">{product.rating} ({product.numReviews} Reviews)</div>
+                  </div>
+                </li>
+              ))
+            }
           </ul>
         </div>
       </main>
       <footer className="footer">
-        All rights reserved.
+        &copy; {new Date().getFullYear()} Amazona company. All rights reserved.
         </footer>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export default {
+    products: [
+        {
+            id: 1,
+            name: 'Journey Edition - Yellow',
+            imageUrl: '/images/bathu.webp',
+            brand: 'Bathu',
+            price: 1.700,
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            id: 2,
+            name: 'Journey Edition - Army Green',
+            imageUrl: '/images/bathu_green.webp',
+            brand: 'Bathu',
+            price: 1.700,
+            rating: 5,
+            numReviews: 20
+        },
+        {
+            id: 3,
+            name: 'Journey Edition - Red',
+            imageUrl: '/images/bathu_red.webp',
+            brand: 'Bathu',
+            price: 1.700,
+            rating: 3.5,
+            numReviews: 13
+        }
+    ]
+}
